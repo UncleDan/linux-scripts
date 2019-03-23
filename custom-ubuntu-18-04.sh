c94571440034939ed3dbd5a5e656732cc796eba3
UncleDan <uncledan@uncledan.it>
2019-03-23 21:19:19 +0100

    testing install Chrome and WineHQ from repo

--- custom-ubuntu-18-04.sh
+++ custom-ubuntu-18-04.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 echo "*** CUSTOMIZING..."
 echo ""
+echo ""
+echo "** Creating temp folder..."
+MYDIR=$(pwd)
+MYTEMPDIR=`mktemp -d`
+if [[ ! "$MYTEMPDIR" || ! -d "$MYTEMPDIR" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+cd $MYTEMPDIR
+echo ""
+echo ""
+echo "** Adding repo for Google Chrome and Wine HQ..."
+echo ""
+echo "* add repos"
+sudo bash -c 'cat << EOF > /etc/apt/sources.list.d/google-chrome.list
+deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
+EOF'
+sudo dpkg --add-architecture i386
+sudo bash -c 'cat << EOF > /etc/apt/sources.list.d/winehq.list
+deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main
+EOF'
+echo ""
+echo "* add keys"
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo apt-key add linux_signing_key.pub
+wget https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+echo ""
+echo ""
 echo "** Update all..."
 echo ""
 echo "* apt clean"
@@ -12,44 +41,42 @@ echo ""
 echo "* apt upgrade"
 sudo apt upgrade -y
 echo ""
+echo ""
 echo "** Uninstalling old versions packages..."
 echo ""
-echo "* apt purge gimp*"
-sudo apt purge -y gimp*
+echo "* apt purge gimp* wine*"
+sudo apt purge -y gimp* wine*
 sudo apt autoclean
+echo ""
 echo ""
 echo "** Install preferred packages..."
 echo ""
-echo "* apt install libreoffice firefox firefox-locale-it thunderbird thunderbird-locale-it keepassx vlc flatpak openssh-server xrdp xorgxrdp git putty wine-stable zip unzip p7zip filezilla snapd"
-sudo apt install -y libreoffice firefox firefox-locale-it thunderbird thunderbird-locale-it keepassx vlc flatpak openssh-server xrdp xorgxrdp git putty wine-stable zip unzip p7zip filezilla snapd
+echo "* apt install libreoffice firefox firefox-locale-it thunderbird thunderbird-locale-it keepassx vlc flatpak openssh-server xrdp xorgxrdp git putty zip unzip p7zip filezilla snapd google-chrome-stable winehq-stable"
+sudo apt install -y --install-recommends libreoffice firefox firefox-locale-it thunderbird thunderbird-locale-it keepassx vlc flatpak openssh-server xrdp xorgxrdp git putty zip unzip p7zip filezilla snapd google-chrome-stable winehq-stable
+echo ""
 echo ""
 echo "** Installing snap software..."
 echo ""
 echo "* snap install notepad-plus-plus"
 sudo snap install notepad-plus-plus
 echo ""
+echo ""
 echo "** Installing flatpak software..."
 echo ""
 echo "* flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref"
 sudo flatpak install -y https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
 echo ""
-echo "** Installing Google Chrome deb package..."
-echo ""
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt install -f
-rm -rf google-chrome-stable_current_amd64.deb
 echo ""
 echo "** Installing Angry IP Scanner deb package..."
 echo ""
-wget http://www.danielelolli.it/files/archive/Software/AngryIPScanner/Linux/ipscan_3.5.5_amd64.deb
+wget http://www.danielelolli.it/files/archive/Software/Linux/ipscan_3.5.5_amd64.deb
 sudo dpkg -i ipscan_3.5.5_amd64.deb
 sudo apt install -f
 rm -rf ipscan_3.5.5_amd64.deb
 echo ""
 echo "** Installing FreeFileSync..."
 echo ""
-wget http://www.danielelolli.it/files/archive/Software/FreeFileSync/Linux/FreeFileSync_10.10_Linux.tar.gz
+wget http://www.danielelolli.it/files/archive/Software/Linux/FreeFileSync_10.10_Linux.tar.gz
 tar -xvzf FreeFileSync_10.10_Linux.tar.gz
 rm -rf FreeFileSync_10.10_Linux.tar.gz
 unzip -o FreeFileSync/Resources.zip -d FreeFileSync/Resources
@@ -85,15 +112,19 @@ Name=RealTimeSync
 EOF
 chmod +x ~/.local/share/applications/RealTimeSync.desktop
 echo ""
+echo ""
 echo "** Installing pCloud AppImage..."
 echo ""
 mkdir -p ~/Software/pCloud
-wget http://www.danielelolli.it/files/archive/Software/pCloud/Linux/2019-02-27/pcloud
-mv pcloud ~/Software/pCloud
+wget http://www.danielelolli.it/files/archive/Software/Linux/pcloud_2019-03-23_amd64
+mv pcloud_2019-03-23_amd64 ~/Software/pCloud/pcloud
 chmod +x ~/Software/pCloud/pcloud
 echo ""
 echo "ATTENZIONE! per attivare pCloud fare doppio click sul relativo eseguibile."
-echo ""
-echo ""
 # ^^^ Still some problems on pcloud.
+cd $MYDIR
+MYDIR=
+MYTEMPDIR=
+echo ""
+echo ""
 echo "DONE."
