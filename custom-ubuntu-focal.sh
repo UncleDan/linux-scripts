365ba2c36c4ec4d6ec54671a3a000017e1688a84
UncleDan <uncledan74@gmail.com>
2020-12-11 18:35:21 +0100

    custom-ubuntu-focal.sh

--- custom-ubuntu-focal.sh
+++ custom-ubuntu-focal.sh
@@ -13,6 +13,10 @@ echo "* apt upgrade"
 sudo apt upgrade -y
 sudo apt autoremove -y
 echo ""
+echo "** Set RTC time to avoid problems in dual boot..."
+echo ""
+sudo timedatectl set-local-rtc 1 --adjust-system-clock
+echo ""
 echo "** Installing Google Chrome deb package..."
 echo ""
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -26,35 +30,50 @@ wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo dpkg -i teamviewer_amd64.deb
 sudo apt --fixbroken install -f
 rm -rf teamviewer_amd64.deb
+echo ""
+echo ""
+echo "** Installing Visual Studio Cpde deb package..."
+echo ""
+wget https://az764295.vo.msecnd.net/stable/940b5f4bb5fa47866a54529ed759d95d09ee80be/code_1.52.0-1607640828_amd64.deb
+sudo dpkg -i code_1.52.0-1607640828_amd64.deb
+rm -rf code_1.52.0-1607640828_amd64.deb
+echo "** Install APT utilities (keepassx vlc openssh-server xrdp xorgxrdp git putty wine-stable zip unzip p7zip-full filezilla)..."
+sudo apt install keepassx vlc openssh-server xrdp xorgxrdp git putty wine-stable zip unzip p7zip-full filezilla -y
+echo ""
+echo "** Remove APT Thunderbird 68.x ..."
+echo ""
+sudo apt remove thunderbird -y
+sudo apt autoremove -y
+echo ""
+echo "** Install Snap Thunderbird 78.x ..."
+echo ""
+sudo snap install thunderbird
+echo ""
+echo "** Remove APT Libreoffice 6.x ..."
+echo ""
+sudo apt-get remove --purge libreoffice*
+sudo apt autoremove -y
+echo ""
+echo "** Install Snap Libreoffice 7.x ..."
+echo ""
+sudo snap install libreoffice
+echo ""
+echo "** Remove APT Gimp (if present) 6.x ..."
+echo ""
+sudo apt-get remove --purge gimp*
+sudo apt autoremove -y
+echo ""
+echo "** Install Snap Gimp ..."
+echo ""
+sudo snap install gimp
+echo "** Install Snap Notepad++"
+sudo snap install notepad-plus-plus
+echo ""
+
+
 
 ### CHECKED ABOVE ^^^ ###
 
-echo ""
-
-echo "** Install preferred packages..."
-echo ""
-echo "* apt install libreoffice firefox firefox-locale-it thunderbird thunderbird-locale-it keepassx vlc openssh-server xrdp xorgxrdp git putty wine-stable zip unzip p7zip filezilla"
-sudo apt install -y libreoffice firefox firefox-locale-it thunderbird thunderbird-locale-it keepassx vlc openssh-server xrdp xorgxrdp git putty wine-stable zip unzip p7zip filezilla
-echo ""
-echo "** Uninstalling old Gimp versions..."
-echo ""
-echo "* apt purge gimp*"
-sudo apt purge -y gimp*
-echo ""
-echo "** Installing flatpak software..."
-echo ""
-echo "* apt install flatpak"
-sudo apt install -y flatpak
-echo "* flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref"
-sudo flatpak install -y https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
-echo ""
-echo "** Installing snap software..."
-echo ""
-echo "* apt install snapd"
-sudo apt install -y snapd
-echo "* snap install notepad-plus-plus"
-sudo snap install notepad-plus-plus
-echo ""
 echo "** Installing Angry IP Scanner deb package..."
 echo ""
 wget https://filedn.com/lAHAHtmqjaTjJxFAtUSMfN8/files/archive/Software/Linux/ipscan_3.6.2_amd64.deb
