83437736834aa264010c2708a311f1902fb35f96
UncleDan <uncledan74@gmail.com>
2021-06-08 16:53:30 +0200

    update

--- custom-ubuntu-focal.sh
+++ custom-ubuntu-focal.sh
@@ -81,61 +81,18 @@ rm -rf ipscan_3.7.3_amd64.deb
 echo ""
 echo "** Installing FreeFileSync..."
 echo ""
-wget https://filedn.com/lAHAHtmqjaTjJxFAtUSMfN8/files/archive/Software/Linux/FreeFileSync_10.20_Linux.tar.gz
-tar -xvzf FreeFileSync_10.20_Linux.tar.gz
-rm -rf FreeFileSync_10.20_Linux.tar.gz
-unzip -o FreeFileSync/Resources.zip -d FreeFileSync/Resources
-mkdir -p ~/Software
-mv FreeFileSync ~/Software
-mkdir -p ~/.local/share/applications
-cat > ~/.local/share/applications/FreeFileSyncSync.desktop <<EOF
-#!/usr/bin/env xdg-open
-
-[Desktop Entry]
-Type=Application
-Name=FreeFileSync
-Name[de]=FreeFileSync
-Name[it]=FreeFileSync
-GenericName=Folder Comparison and Synchronization
-GenericName[de_DE]=Ordnervergleich und Synchronisation
-GenericName[it_IT]=Comparazione e Sincronizzazione di Cartelle
-Exec=/home/carlotta/Software/FreeFileSync/FreeFileSync %F
-Icon=/home/carlotta/Software/FreeFileSync/Resources/FreeFileSync.png
-Icon[de]=/home/carlotta/Software/FreeFileSync/Resources/FreeFileSync.png
-Icon[it]=/home/carlotta/Software/FreeFileSync/Resources/FreeFileSync.png
-NoDisplay=false
-Terminal=false
-Categories=Utility;FileTools;
-StartupNotify=true
-EOF
-chmod +x ~/.local/share/applications/FreeFileSyncSync.desktop
-cat > ~/.local/share/applications/RealTimeSync.desktop <<EOF
-#!/usr/bin/env xdg-open
-
-[Desktop Entry]
-Type=Application
-Name=RealTimeSync
-Name[de]=RealTimeSync
-Name[it]=RealTimeSync
-GenericName=Automated Synchronization
-GenericName[de_DE]=Automatisierte Synchronisation
-GenericName[it_IT]=Sincronizzazione Automatica
-Exec=/home/carlotta/Software/FreeFileSync/RealTimeSync %f
-Icon=/home/carlotta/Software/FreeFileSync/Resources/RealTimeSync.png
-Icon[de]=/home/carlotta/Software/FreeFileSync/Resources/RealTimeSync.png
-Icon[it]=/home/carlotta/Software/FreeFileSync/Resources/RealTimeSync.png
-NoDisplay=false
-Terminal=false
-Categories=Utility;FileTools;
-StartupNotify=true
-EOF
-chmod +x ~/.local/share/applications/RealTimeSync.desktop
+wget https://filedn.com/lAHAHtmqjaTjJxFAtUSMfN8/files/archive/Software/Linux/FreeFileSync_11.10_Linux.tar.gz
+tar -xvzf FreeFileSync_11.10_Linux.tar.gz
+rm -rf FreeFileSync_11.10_Linux.tar.gz
+./FreeFileSync_11.10_Install.run
+rm -rf FreeFileSync_11.10_Install.run
+# ^^^ Check if FreeFileSync can be completely silent
 echo ""
 echo "** Installing pCloud AppImage..."
 echo ""
 mkdir -p ~/Software/pCloud
-wget https://filedn.com/lAHAHtmqjaTjJxFAtUSMfN8/files/archive/Software/Linux/pcloud_2020-03-08_amd64.AppImage
-mv pcloud_2020-03-08_amd64.AppImage ~/Software/pCloud/pcloud
+wget https://filedn.com/lAHAHtmqjaTjJxFAtUSMfN8/files/archive/Software/Linux/pcloud_2021-06-08_amd64.AppImage
+mv pcloud_2021-06-08_amd64.AppImage ~/Software/pCloud/pcloud
 chmod +x ~/Software/pCloud/pcloud
 echo ""
 echo "ATTENZIONE! per attivare pCloud fare doppio click sul relativo eseguibile."
@@ -143,5 +100,5 @@ echo "Se non funziona riscaicare AppImage aggiornata da:"
 echo "https://www.pcloud.com/it/how-to-install-pcloud-drive-linux.html"
 echo ""
 echo ""
-# ^^^ Still some problems on pcloud.
+# ^^^ Still some problems on pcloud, it would be nice to have it completely automatic.
 echo "DONE."
