c2fec55f3e7a0edc46ebbf8494efb771fa13157e
UncleDan <github@uncledan.it>
2022-04-19 18:36:09 +0200

    update fedora 39 create centos 9

--- custom-centos-9-stream.sh
+++ custom-centos-9-stream.sh
@@ -4,8 +4,19 @@ echo ""
 echo "** Update all..."
 echo ""
 echo "* Update RPM and flatpak packages"
+echo ""
 sudo dnf upgrade -y
 sudo flatpak update
+echo ""
+echo "* Install EPEL repositories..."
+echo ""
+sudo dnf install \
+    https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm \
+    https://dl.fedoraproject.org/pub/epel/epel-next-release-latest-9.noarch.rpm
+echo ""
+echo "* Install NTFS support..."
+echo ""
+sudo dnf install ntfs-3g -y
 echo ""
 echo "** Installing Google Chrome RPM package..."
 echo ""
@@ -26,20 +37,22 @@ sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.m
 dnf check-update -y
 sudo dnf install code -y
 echo ""
-echo "** Installing VLC RPM package..."
+echo "** Installing VLC flatpak package..."
 echo ""
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install org.videolan.VLC -y
+flatpak run org.videolan.VLC
 echo ""
 echo "** Installing multimedia codecs and plugins RPM package..."
 echo ""
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} --exclude=gstreamer1-plugins-bad-free-devel -y
+### STILL TO COME gstreamer1-plugin-openh264 gstreamer1-libav - REQUIRED??? 
 sudo dnf install lame\* --exclude=lame-devel -y
 sudo dnf group upgrade --with-optional Multimedia -y
 echo ""
 echo "** OBS flatpak package..."
 echo ""
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.obsproject.Studio -y
+sudo flatpak install flathub com.obsproject.Studio -y
 flatpak run com.obsproject.Studio
 echo ""
 echo "** Installing Zoom RPM package..."
@@ -53,8 +66,16 @@ echo ""
 wget https://binaries.webex.com/WebexDesktop-CentOS-Official-Package/Webex.rpm
 sudo dnf localinstall ./Webex.rpm -y
 rm -rf ./Webex.rpm
+echo ""
 echo "** Installing Mozilla Thunderbird and other utilities RPM packages..."
-sudo dnf install keepassxc openssh-server git putty zip unzip p7zip p7zip-plugins thunderbird -y
+echo ""
+sudo dnf install openssh-server git zip unzip p7zip p7zip-plugins thunderbird -y
+echo ""
+echo "** Installing Webex RPM package..."
+echo ""
+sudo flatpak install org.keepassxc.KeePassXC -y
+flatpak run org.keepassxc.KeePassXC
+### STILL TO COME??? putty
 echo ""
 echo "** Remove Libreoffice RPM old packages and install Libreoffice RPM latest packages package..."
 echo ""
@@ -88,6 +109,7 @@ firefox --new-instance --private-window "https://www.pcloud.com/how-to-install-p
 sudo rm -rf /opt/pcloud
 sudo mkdir -p /opt/pcloud
 sudo mv ~/Scaricati/pcloud /opt/pcloud/
+sudo chown -R root:root/opt/pcloud
 sudo chmod -R 755 /opt/pcloud
 /opt/pcloud/pcloud
 echo ""
@@ -102,11 +124,12 @@ rm -rf FreeFileSync_11.20_Install.run
 echo ""
 echo "** Install wine and Notepad++"
 echo ""
-sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/35/winehq.repo
-sudo dnf install winehq-stable -y
-wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.3.3/npp.8.3.3.Installer.x64.exe
-wine ./npp.8.3.3.Installer.x64.exe
-rm -rf ./npp.8.3.3.Installer.x64.exe
+### STILL TO COME 
+### sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/34/winehq.repo
+### sudo dnf install winehq-stable -y
+### wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.3.3/npp.8.3.3.Installer.x64.exe
+### wine ./npp.8.3.3.Installer.x64.exe
+### rm -rf ./npp.8.3.3.Installer.x64.exe
 # ^^^ Check why notepad++ runs only from console by running "wine ~/.wine/drive_c/Program\ Files/Notepad++/notepad++.exe"
 sudo dnf autoremove -y
 echo "DONE."
