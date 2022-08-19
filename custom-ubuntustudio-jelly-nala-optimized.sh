bc36eba87a91af4767125e8ae343afd4e6c62b9b
UncleDan <github@uncledan.it>
2022-08-19 12:37:20 +0200

    ubuntu nala optimized

--- custom-ubuntustudio-jelly-nala-optimized.sh
+++ custom-ubuntustudio-jelly-nala-optimized.sh
@@ -11,10 +11,10 @@ echo ""
 ## Hints from: https://mutschler.dev/linux/ubuntu-btrfs-20-04/
 sudo systemctl enable fstrim.timer
 
-echo "Install nala package manager..."
+echo "Install nala package manager and basic utilities..."
 echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list > /dev/null
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-sudo apt update && sudo apt install nala -y
+sudo apt update && sudo apt install nala curl git gpg wget -y
 
 ## Again from: https://mutschler.dev/linux/ubuntu-btrfs-20-04/
 echo "** Installing and running Timeshift, on first run choose your options..."
@@ -22,21 +22,25 @@ echo ""
 sudo nala install timeshift -y
 sudo timeshift-gtk
 
+## Temporary folder
+tmp_dir=$(mktemp -d -t ci-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX)
+
 echo ""
 echo "** Installing timeshift-autosnap-apt..."
 echo ""
-rm -rf ~/timeshift-autosnap-apt
-git clone https://github.com/wmutschl/timeshift-autosnap-apt.git ~/timeshift-autosnap-apt
-cd ~/timeshift-autosnap-apt
+rm -rf $tmp_dir/timeshift-autosnap-apt
+git clone https://github.com/wmutschl/timeshift-autosnap-apt.git $tmp_dir/timeshift-autosnap-apt
+cd $tmp_dir/timeshift-autosnap-apt
 sudo make install
 
 echo ""
 echo "** Installing timeshift-autosnap-apt..."
 echo ""
-rm -rf ~/grub-btrfs
-git clone https://github.com/Antynea/grub-btrfs.git 
-cd ~/grub-btrfs
+rm -rf $tmp_dir/grub-btrfs
+git clone https://github.com/Antynea/grub-btrfs.git $tmp_dir/grub-btrfs
+cd $tmp_dir/grub-btrfs
 sudo make install
+
 cd ~
 
 echo ""
@@ -44,102 +48,101 @@ echo "** INTERNET: Installing Google Chrome DEB package from repository..."
 echo ""
 wget -qO - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/google-linux-signing-key.gpg > /dev/null
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list > /dev/null
-sudo nala update && sudo nala install google-chrome-stable -y
 
 echo ""
 echo "** INTERNET: Installing Teamviewer DEB package from repository..."
 echo ""
 sudo wget -q https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc && sudo mv TeamViewer2017.asc /etc/apt/trusted.gpg.d/TeamViewer2017.asc
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/TeamViewer2017.asc] https://linux.teamviewer.com/deb stable main" | sudo tee /etc/apt/sources.list.d/teamviewer.list > /dev/null
-sudo nala update && sudo nala install teamviewer -y
 
 echo ""
 echo "** INTERNET: Installing Anydesk DEB package via repository..."
 echo ""
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/anydesk.gpg > /dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/anydesk.gpg] http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null
-sudo nala update && sudo nala install anydesk -y
 
 echo ""
 echo "** UTILITY: Install Visual Studio Code DEB package via MS repository..."
 echo ""
 wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/packages.microsoft.gpg > /dev/null
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-sudo nala update && sudo nala install code -y
-
 echo ""
 echo "** VIRTUALIZATION: Installing Oracle Virtualbox DEB package via repository..."
 echo ""
 wget -qO - https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor  | sudo tee /etc/apt/trusted.gpg.d/oracle-virtualbox-2016.gpg > /dev/null
 echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib" | sudo tee /etc/apt/sources.list.d/oracle-virtualbox.list > /dev/null
-sudo nala update && sudo nala install virtualbox-6.1 -y
 
 echo ""
-echo "** Installing curl, git, zip, unzip, 7zip and flatpak commands..."
+echo "** Installing zip, unzip, 7zip and flatpak commands..."
 echo ""
-sudo nala update && sudo nala install curl git zip unzip p7zip-full flatpak -y
 
 echo ""
 echo "** INTERNET: Installing Mozilla Thunderbird italian localization DEB package via repository..."
 echo ""
-sudo nala update && sudo nala install thunderbird-locale-it -y
 
 echo ""
 echo "** OFFICE: Installing Libreoffice localization DEB package..." ## Jelly is up to date to 7.3.2.2 as of 26/04/2022, if not Italian guide is here https://wiki.documentfoundation.org/Documentation/Install/Linux/it
 echo ""
-sudo nala update && sudo nala install libreoffice-l10n-it hyphen-it mythes-it libreoffice-help-it hunspell hunspell-it -y ## myspell-it has no candidates
-
-## Temporary folder
-tmp_dir=$(mktemp -d -t ci-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX)
 
 echo ""
 echo "** INTERNET: Installing Zoom DEB package..."
 echo ""
 curl -L https://zoom.us/client/latest/zoom_amd64.deb -o $tmp_dir/zoom_amd64.deb
-sudo nala install $tmp_dir/zoom_amd64.deb -y
 
 echo ""
 echo "** INTERNET: Installing Webex DEB package..."
 echo ""
 curl -L https://binaries.webex.com/WebexDesktop-Ubuntu-Official-Package/Webex.deb -o $tmp_dir/Webex.deb
-sudo nala install $tmp_dir/Webex.deb -y
 
 echo ""
 echo "** OFFICE: Installing Onlyoffice Desktop Editors DEB package..."
 echo ""
 curl -L https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb -o $tmp_dir/onlyoffice-desktopeditors_amd64.deb
-sudo nala install $tmp_dir/onlyoffice-desktopeditors_amd64.deb -y
 
 echo ""
 echo "** UTILITY: Installing Angry IP scanner DEB package..."
 echo ""
 curl -L https://github.com/angryip/ipscan/releases/download/3.8.2/ipscan_3.8.2_amd64.deb -o $tmp_dir/ipscan_3.8.2_amd64.deb
-sudo nala install $tmp_dir/ipscan_3.8.2_amd64.deb -y
 
 echo ""
 echo "** UTILITY: Install KeepassXC DEB package..."
 echo ""
-sudo nala update && sudo nala install keepassxc -y
 
 echo ""
 echo "** UTILITY: Install PUTTY DEB package..."
 echo ""
-sudo nala update && sudo nala install putty putty-doc -y
 
 echo ""
 echo "** UTILITY: Install Filezilla DEB package..."
 echo ""
-sudo nala update && sudo nala install filezilla -y
 
 echo ""
 echo "** UTILITY: Install OpenSSH Server DEB package..."
 echo ""
-sudo nala update && sudo nala install openssh-server -y
 
 echo ""
 echo "** UTILITY: Installing wine for Notepad++"
 echo ""
-sudo nala update && sudo nala install wine-stable -y
+
+sudo nala update
+sudo nala install -y \
+ google-chrome-stable \
+ teamviewer \
+ anydesk \
+ code \
+ virtualbox-6.1 \
+ zip unzip p7zip-full flatpak \
+ thunderbird-locale-it \
+ libreoffice-l10n-it hyphen-it mythes-it libreoffice-help-it hunspell hunspell-it \
+ $tmp_dir/zoom_amd64.deb \
+ $tmp_dir/Webex.deb \
+ $tmp_dir/onlyoffice-desktopeditors_amd64.deb \
+ $tmp_dir/ipscan_3.8.2_amd64.deb \
+ keepassxc \
+ putty putty-doc \
+ filezilla \
+ openssh-server \
+ wine-stable
 
 echo ""
 echo "INTERNET: Remove Firefox snap and snapd..."
@@ -155,12 +158,14 @@ sudo systemctl disable snapd.socket
 sudo systemctl disable snapd.seeded.service
 sudo rm -rf /var/cache/snapd/
 rm -rf ~/snap
-sudo nala purge snapd -y
 
 echo ""
 echo "** GRAPHICS: Removing GIMP DEB package..."
 echo ""
-sudo nala purge gimp* -y
+
+sudo nala purge -y \
+ snapd \
+ gimp gimp-*
 
 echo ""
 echo "INTERNET: Installing Firefox flatpak package..."
@@ -171,7 +176,6 @@ sudo flatpak install flathub org.mozilla.firefox
 echo ""
 echo "** GRAPHICS: Installing GIMP flatpak package..."
 echo ""
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak -y install flathub org.gimp.GIMP
 
 echo ""
