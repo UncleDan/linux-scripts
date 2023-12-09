4a147886a5d9bbdee89848d37442a2eabeaecc63
UncleDan <github@uncledan.it>
2023-12-09 19:34:38 +0100

    master cleanup

--- thumbsup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker run -t -v /home/test/dl-gallery/dl-gallery_sources:/input:ro -v /home/test/dl-gallery/thumbsup:/output -u $(id -u):$(id -g) thumbsupgallery/thumbsup thumbsup --input /input --output /output --sort-albums-by "title" --title "UncleDan's Gallery" --footer "Copyright 2004-2019 Daniele Lolli (UncleDan)"
-sudo rsync -azvr /home/test/dl-gallery/thumbsup/* /var/www/html --delete
